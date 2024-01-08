a25ca067c6194fac43d258a9be682f31a6813e7c
Steven Mentzer <stevenfmentzer@gmail.com>
2024-01-08 15:27:47 -0500

    done?

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+    while countdown >= 0:
+        print(countdown)
+        countdown-=1 
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared = []
+    for num in int_list:
+        num_square = num * num
+        squared.append(num_square)
+    return squared
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    count = 1
+    while count < 101: 
+        if count % 3 == 0 and count % 5 == 0: 
+            print("FizzBuzz")
+        elif count % 3 == 0: 
+            print("Fizz")
+        elif count % 5 == 0: 
+            print("Buzz")
+        else: 
+            print(count)
+        count -= 1
